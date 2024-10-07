b8104c2c6e91d695d514fd440265b1606fcd5be2
Damian Savage <savage.damian@outlook.com>
2024-10-07 03:41:56 -0500

    Updated for Assignment 3

--- scripts/assignment3.js
+++ scripts/assignment3.js
@@ -1,2 +1,44 @@
+console.log("Welcome To My Assignment 3");
+const Answer = document.getElementById("answer-box")
+
+function convertTemp(){
+    let scale = prompt("Press C for converting Celsius to Fahrenheit, or Press F for converting Fahrenheit to Celsius")
+    if(scale === "C"){
+        function convertC(){
+            let temperature = prompt("Enter a temperature in Celsius that you wish to convert");
+            let Cl = (temperature*9/5)+32;
+            Answer.innerHTML = ("The conversion of " + temperature + "° Celsius to Fahrenheit is " + Cl + "°");
+            return temperature;  
+        }
+        convertC();
+    }else if (scale === "c"){
+        function convertC(){
+            let temperature = prompt("Enter a temperature in Celsius that you wish to convert");
+            let Cl = (temperature*9/5)+32;
+            Answer.innerHTML = ("The conversion of " + temperature + "° Celsius to Fahrenheit is " + Cl + "°");
+            return temperature;  
+        }
+        convertC();
+    }else if (scale === "F"){
+        function convertF(){
+            let American = prompt("Enter a temperature in Fahrenheit that you wish to convert");
+            let Fr = (American-32)*5/9;
+            Answer.innerHTML = ("The conversion of " + American + "° Fahrenheit to Celsius is " + Fr + "°");
+            return temperature; 
+        }
+        convertF();
+    }else if (scale === "f"){
+        function convertF(){
+            let American = prompt("Enter a temperature in Fahrenheit that you wish to convert");
+            let Fr = (American-32)*5/9;
+            Answer.innerHTML = ("The conversion of " + American + "° Fahrenheit to Celsius is " + Fr + "°");
+            return temperature; 
+    }
+    convertF();
+    }else{
+        Answer.innerHTML = ("You have entered an invalid Selection"); 
+    }
+    }
+
 
 
