bf0124a63d4d418355e81a7b963b5fe553a2f2c6
Damian Savage <savage.damian@outlook.com>
2024-10-07 02:36:45 -0500

    Fixed Assignment2

---
+++ scripts/assignment2.js
@@ -0,0 +1,10 @@
+console.log("Assignment 2");
+const Answer = document.getElementById("answer-box")
+
+function convertTemp(){
+    let temperature = prompt("Enter a temperature in Celcius");
+    let C = temperature*1.8+32;
+    Answer.innerHTML = ("The conversion of " + temperature + "° Celsius to Fahrenheit is " + C + "°");
+    return temperature;
+}
+
